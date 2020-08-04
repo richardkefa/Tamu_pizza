d80243ed11120119d5f4fe74c06c4718b078cfcb
richardkefa <richardkefa@gmail.com>
2020-08-04 09:42:10 +0300

    remove bugs and add contacrs

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,44 @@
-$(document).ready(function(){
-  function Order(type,crust,topping,size,quantity){
-    this.pizzatype=type;
-    this.pizzacrust=crust;
-    this.pizzatopping=topping;
-    this.pizzasize=size;
-    this.pizzaquantity=quantity;
-    this.deliveryaddress=[];
+$(document).ready(function () {
+  //Bussiness logic
+  function Order(type, crust, topping, size, quantity) {
+    this.pizzatype = type;
+    this.pizzacrust = crust;
+    this.pizzatopping = topping;
+    this.pizzasize = size;
+    this.pizzaquantity = quantity;
+    this.deliveryaddress = [];
   };
-  function Delivery(name,phone,location){
-    this.customerName=name;
-    this.customerPhone=phone;
-    this.delivaryLocation=location;
+  function Delivery(name, phone, location) {
+    this.customerName = name;
+    this.customerPhone = phone;
+    this.delivaryLocation = location;
   };
 
 
 
 
   //User inerface logic
-  $('input[name=delivery]').change(function(){
-    if($(this).is(':checked')) {
+  $('input[name=delivery]').change(function () {
+    if ($(this).is(':checked')) {
       $("#delivery-address").show();
     } else {
       $("#delivery-address").hide();
     }
-});
-  $("#checkoutbtn").click(function(event){
-    var pizzatype =$("#pizzatype option:selected").val();
-    var pizzacrust =$("#pizzacrust option:selected").val();
-    var pizzatopping=$("#pizzatopping option:selected").val();
-    var pizzasize =$("#pizzasize option:selected").val();
-    var pizzaquantity =parseInt(document.getElementById("quantity").value);
-    var customerName=$("input#name").val();
-    var customerPhone=document.getElementById("phonenumber").value;
-    var customerLocation=$("input#location").val();
-    var newOrder= new Order(pizzatype,pizzacrust,pizzatopping,pizzasize,pizzaquantity);
-    var newAddress= new Delivery(customerName,customerPhone,customerLocation);
+  });
+  $("#checkoutbtn").click(function (event) {
+    var pizzatype = $("#pizzatype option:selected").val();
+    var pizzacrust = $("#pizzacrust option:selected").val();
+    var pizzatopping = $("#pizzatopping option:selected").val();
+    var pizzasize = $("#pizzasize option:selected").val();
+    var pizzaquantity = parseInt(document.getElementById("quantity").value);
+    var customerName = $("input#name").val();
+    var customerPhone = document.getElementById("phonenumber").value;
+    var customerLocation = $("input#location").val();
+    var newOrder = new Order(pizzatype, pizzacrust, pizzatopping, pizzasize, pizzaquantity);
+    var newAddress = new Delivery(customerName, customerPhone, customerLocation);
     newOrder.deliveryaddress.push(newAddress);
     console.log(customerPhone)
-    
+
 
     $("#checkout").show();
     $(".pizza-type").text(newOrder.pizzatype);
@@ -45,67 +46,67 @@ $(document).ready(function(){
     $(".pizza-topping").text(newOrder.pizzatopping);
     $(".pizza-size").text(newOrder.pizzasize);
     $(".pizza-quantity").text(newOrder.pizzaquantity);
-    newOrder.deliveryaddress.forEach(function(address){
-      $(".delivery-info").text(address.customerName+" "+ address.customerPhone +" "+address.delivaryLocation)
+    newOrder.deliveryaddress.forEach(function (address) {
+      $(".delivery-info").text(address.customerName + " " + address.customerPhone + " " + address.delivaryLocation)
     })
-    
 
-    switch (pizzasize){
+
+    switch (pizzasize) {
       case "large":
-       var price=1330;
-      break;
+        var price = 1330;
+        break;
       case "medium":
-        price=1000;
-      break;
+        price = 1000;
+        break;
       case "small":
-        price=600;
-      break;
+        price = 600;
+        break;
       default:
         alert("select your pizza size");
     }
-    switch(pizzacrust){
+    switch (pizzacrust) {
       case "cripsy":
-       var crust_price=200;
-      break;
+        var crust_price = 200;
+        break;
       case "stuffed":
-        crust_price=150;
+        crust_price = 150;
         break;
       case "glutenfree":
-        crust_price=300;
-      break;
+        crust_price = 300;
+        break;
       default:
         alert("select your crust");
     }
-    switch(pizzatopping){
+    switch (pizzatopping) {
       case "mushrooms":
-       var topping_cost=200;
-      break;
+        var topping_cost = 200;
+        break;
       case "onions":
-        topping_cost=100
-      break;
+        topping_cost = 100
+        break;
       case "extracheese":
-        topping_cost=250;
-      break;
+        topping_cost = 250;
+        break;
       default:
         alert("select your topping");
     }
-    var total=(price+crust_price+topping_cost)*pizzaquantity;
-    
-    if (document.getElementById("delivery").checked){
-      var checkoutTotal=total+300;
+    var total = (price + crust_price + topping_cost) * pizzaquantity;
+
+    if (document.getElementById("delivery").checked) {
+      var checkoutTotal = total + 300;
       $(".pizza-cost").text(checkoutTotal);
       $("#deliveryinfo").show();
     }
     else {
       $(".pizza-cost").text(total);
     }
-    $("#confirmorder").click(function(event){
+    $("#confirmorder").click(function (event) {
       alert("we have recieved your order");
       $("#checkout").hide();
-      
+      event.preventDefault();
     })
-    
+
     event.preventDefault();
   });
-  
+
 })
